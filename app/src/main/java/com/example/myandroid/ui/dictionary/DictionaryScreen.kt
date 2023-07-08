2a2c1ec4deef1a7feb54ae36d6a04b80c114c283
hatto <ryosuke.10117@gmail.com>
2023-07-09 07:30:44 +0900

    Initial Commit

---
+++ app/src/main/java/com/example/myandroid/ui/dictionary/DictionaryScreen.kt
@@ -0,0 +1,18 @@
+package com.example.myandroid.ui.dictionary
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DictionaryScreen() {
+
+}
+
+@Composable
+fun SearchBar() {
+
+}
+
+@Composable
+fun WordDescriptionCard() {
+
+}
---
+++ app/src/main/java/com/example/myandroid/ui/dictionary/DictionaryViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.myandroid.ui.dictionary
+
+import androidx.lifecycle.ViewModel
+
+class DictionaryViewModel : ViewModel() {
+
+}
